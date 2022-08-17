affacbe3012b023924a69e660b86d7656e3868f9
Tomas <Tomas@LAPTOP-QAQ7DTUK>
2022-08-17 01:15:55 -0300

    fix: finished assignment

---
+++ clase2-desafio/User.js
@@ -0,0 +1,32 @@
+export class User {
+    constructor(name, surname, books, pets) {
+        this.name = name
+        this.surname = surname
+        this.books = books
+        this.pets = pets
+    }
+
+    getFullName() {
+        return this.name + this.surname
+    }
+
+    addPet(pet) {
+        this.pets.push(pet)
+    }
+    countMascota() {
+        return this.pets.length
+    }
+    
+    addBook(name, author) {
+        this.books.push({ name, author })
+    }
+
+    getBooksName() {
+        const booksName = []
+        for (let i=0; i<this.books.length; i++) {
+            booksName.push(this.books[i].name)
+        }
+        return booksName
+    }
+
+}
---
+++ clase2-desafio/index.js
@@ -0,0 +1,52 @@
+class User {
+    constructor(name, surname, books, pets) {
+        this.name = name
+        this.surname = surname
+        this.books = books
+        this.pets = pets
+    }
+
+    getFullName() {
+        return `${this.name} ${this.surname}`;
+    }
+
+    addPet(pet) {
+        this.pets.push(pet)
+    }
+    countMascota() {
+        return this.pets.length
+    }
+    
+    addBook(name, author) {
+        this.books.push({ name, author })
+    }
+
+    getBooksName() {
+        const booksName = []
+        for (let i=0; i<this.books.length; i++) {
+            booksName.push(this.books[i].name)
+        }
+        return booksName
+    }
+
+}
+
+const main = () => {
+    const usuario = new User('tomas', 'vigon',
+     [ {name: "cosmos", author: "messi"}, {name: "matilda", author: "albert einstein"} ],
+     ["cooper", "nala", "hera", "deli"]);
+
+    console.log(usuario);
+    console.log(usuario.countMascota());
+    usuario.addPet('lucas');
+    console.log(usuario.countMascota());
+    console.log(usuario);
+    console.log(usuario.getFullName());
+    console.log(usuario.getBooksName());
+    console.log(usuario.addBook("la mano de dios", "yo"));
+    console.log(usuario);
+    console.log(usuario.getBooksName());
+
+}
+
+main();
