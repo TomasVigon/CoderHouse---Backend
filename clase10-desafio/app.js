20adfe6e5c0018c8a645473fca9250b7c3046aac
Tomas <Tomas@LAPTOP-QAQ7DTUK>
2022-09-15 22:15:41 -0300

    desafio10 pug

--- clase10-desafio/app.js
+++ clase10-desafio/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use('/products', productsRouter);
 app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     res.render('home')
