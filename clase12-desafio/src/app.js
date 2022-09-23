cd0972878b217d274a2b113709e550057b300c54
Tomas <Tomas@LAPTOP-QAQ7DTUK>
2022-09-22 21:20:55 -0300

    fix: commit 8

--- clase12-desafio/src/app.js
+++ clase12-desafio/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Server } = require('socket.io')
 const handlebars = require('express-handlebars')
 const productsRouter = require('./routes/products')
 let products = require('./models/product.model')
@@ -7,7 +8,9 @@ const app = express()
 
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+const io = new Server(server)
 
+app.use(express.static('./src/public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use('/products', productsRouter);
@@ -18,4 +21,8 @@ app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
     res.render('home', {products})
+})
+
+io.on('connection', socket => {
+    console.log(`New user connected: ${socket.id}`)
 })
